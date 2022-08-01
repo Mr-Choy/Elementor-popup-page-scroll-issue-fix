dd71b5e1166e33d7745a9bede8e211801338c400
Mr. CHOY <cy.cy90@gmail.com>
2022-08-01 16:12:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+// when popup open  
+jQuery( document ).on( 'elementor/popup/show', () => {
+  jQuery("html,body").addClass("cantScroll");  
+});      
 
+// when popup close
+jQuery(document).on( 'elementor/popup/hide', () => {
+  jQuery("html,body").removeClass("cantScroll");
+});
